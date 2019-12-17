e77eb0f156ba414ea2ade0112fd4c62e0f59b537
AyselDalcicek <ayseldalcicek07@gmail.com>
2019-12-17 14:11:04 -0800

    "first tests commit"

--- src/test/java/com/vytrack/runners/CucumberRunner.java
+++ src/test/java/com/vytrack/runners/CucumberRunner.java
@@ -6,8 +6,17 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "src\\test\\resources\\features",
-        glue = "com/vytrack/step_definitions"
+        features = "src\\test\\resources\\features",  // path to features
+        glue = "com/vytrack/step_definitions",  // path to definitions
+        dryRun = false,
+       // tags = "",
+        tags = "@store_manager",
+        // tags = "@negative_test"  // Only those test are executed which has @negative_test annotation
+        // tags = "~@negative_test"   // OR   "not @negative_test"    All tests will execute except those test  which has @negative_test annotation
+        // tags = "~@negative_test @driver"   // all tests will execute
+        plugin = {"html:target/default-cucumber-reports"}
+
+
 )
 public class CucumberRunner {
 
