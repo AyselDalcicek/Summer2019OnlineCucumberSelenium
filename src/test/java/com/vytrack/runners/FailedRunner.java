a61c257631d495bc35366949cae7b7e06fe933b9
AyselDalcicek <ayseldalcicek07@gmail.com>
2020-01-04 21:43:36 -0800

    "Parallel testing "

--- src/test/java/com/vytrack/runners/FailedRunner.java
+++ src/test/java/com/vytrack/runners/FailedRunner.java
@@ -1,8 +1,5 @@
 package com.vytrack.runners;
 
-
-
-
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
@@ -11,11 +8,9 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "@target/rerun.txt",
         glue = "com/vytrack/step_definitions",
-        plugin = {"html:target/default-cucumber-reports",
-                "json:target/cucumber.json"
+        plugin = {
+                "html:target/rerun-default-cucumber-reports",
         }
 )
 public class FailedRunner {
-
-
 }
